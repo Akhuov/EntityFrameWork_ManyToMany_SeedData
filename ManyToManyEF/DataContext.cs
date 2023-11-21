0584e4b0d64aefa4ca1a832d4c06aafbef567041
user <user@WIN-F7NIMF7A3VO>
2023-11-21 07:47:41 +0500

    Добавьте файлы проекта.

---
+++ ManyToManyEF/DataContext.cs
@@ -0,0 +1,86 @@
+﻿using ManyToManyEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManyToManyEF
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Staff> Staffs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Staffs;Integrated Security=True;TrustServerCertificate=true;");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            AddData(modelBuilder);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void AddData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Staff>().HasData(
+                new Staff()
+                {
+                    Id = 1,
+                    Name = "Hr bo'limi"
+
+                },
+                new Staff()
+                {
+                    Id = 2,
+                    Name = "Moliya bo'limi"
+                },
+                new Staff()
+                {
+                    Id = 3,
+                    Name = "O'quv bo'limi"
+                }
+            );
+
+            modelBuilder.Entity<User>().HasData(
+                new User()
+                {
+                    Id = 1,
+                    Name = "Rustam",
+                    StaffId = 1,
+                },
+                new User()
+                {
+                    Id = 2,
+                    Name = "Dilmurod",
+                    StaffId = 2
+                },
+                 new User()
+                 {
+                     Id = 3,
+                     Name = "Sanjar",
+                     StaffId = 2
+                 }
+                );
+
+            modelBuilder.Entity<Book>().HasData(new Book()
+            {
+                Id = 1,
+                Name = "O'tkan kunlar"
+            },
+
+            new Book()
+            {
+                Id = 2,
+                Name = "Ikki eshik orasi"
+            },
+            new Book()
+            {
+                Id = 3,
+                Name = "Chinor"
+            });
+        }
+    }
+}
---
+++ ManyToManyEF/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using ManyToManyEF;
+using Microsoft.EntityFrameworkCore;
+
+DataContext dataContext = new DataContext();
+
+
+//var staff=dataContext.Staffs.Include(u=>u.Users).FirstOrDefault(s=>s.Id==2);
+
+//Console.WriteLine("Bulimi nomi: " +staff.Name);
+
+//foreach(var user in staff.Users)
+//{
+//    Console.WriteLine("Ishchilar:"+user.Name);
+//}
+
+
+var user=dataContext.Users.FirstOrDefault(u=>u.Id==2);
+
+var books = dataContext.Books.Where(b => b.Id <= 2).ToList();
+
+
+user.Books=books;
+dataContext.SaveChanges();
+
+
+
+
+
+var Dilmurod=dataContext.Users.Include(b=>b.Books).FirstOrDefault(user=>user.Id==2);
+
+Console.WriteLine($"Yozuvchining ismi:{Dilmurod.Name}");
+
+Console.WriteLine("Uning yozgan kitoblari:");
+foreach (var item in Dilmurod.Books)
+{
+    Console.WriteLine(item.Name);
+}
+{
+
+}
